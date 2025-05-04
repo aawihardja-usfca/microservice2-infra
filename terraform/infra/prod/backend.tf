f55362db30de31a327e8b1ef1ec56a3446fed07b
Andrew Wihardja <andrewadi96@gmail.com>
2025-05-03 19:13:31 -0700

    added terraform for prod cluster resources

---
+++ terraform/infra/prod/backend.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "035863456454-terraform-state-backend"
+    key            = "terraform/prod/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform_state_lock"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
---
+++ terraform/infra/prod/variables.tf
@@ -0,0 +1,41 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+  default     = "prod-cluster"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "eks_cluster_role_arn" {
+  description = "ARN of existing IAM role for EKS cluster"
+  type        = string
+  default     = "arn:aws:iam::035863456454:role/LabRole"
+}
+
+variable "eks_node_group_role_arn" {
+  description = "ARN of existing IAM role for EKS node group"
+  type        = string
+  default     = "arn:aws:iam::035863456454:role/LabRole"
+}
