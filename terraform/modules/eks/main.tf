8ae0dd7cfd4ba6b0128650e113981ef736a7096e
Andrew Wihardja <andrewadi96@gmail.com>
2025-05-03 19:11:29 -0700

    add workflow to deploy to prod

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -93,6 +93,22 @@ resource "aws_security_group" "cluster" {
   description = "EKS control-plane security group"
   vpc_id      = aws_vpc.this.id
 
+  # Allow HTTPS
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
